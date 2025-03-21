67dccbab00da1ce714dee7c2f5b37d3816308713
renrutardnaxela <a.c.t.123.at@gmail.com>
2025-03-21 12:40:46 +0000

    the image now changes!

--- website/script.js
+++ website/script.js
@@ -1,7 +1,7 @@
 function onHover() {
-    myImgID.attr('src', '../assets/silly-alex-contact.jpeg');
+    $("#myImgID").attr('src', '../assets/silly alex contact.jpeg');
 }
 
 function offHover() {
-    myImgID.attr('src', '../assets/alex-turner-conatct.jpeg');
+    $("#myImgID").attr('src', '../assets/alex turner conatct.jpeg');
 }
